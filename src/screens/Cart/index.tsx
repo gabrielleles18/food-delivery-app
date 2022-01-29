e1feda473d00024f29db65a3ba84ceb4bbc8e65c
gabrielleles18 <gabriel.leles18@gmail.com>
2022-01-29 19:56:18 -0300

    Screen cart incomplet

--- src/screens/Cart/index.tsx
+++ src/screens/Cart/index.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
-import {Image, ScrollView, Text, View} from "react-native";
+import {Image, ScrollView, Text, View, Button} from "react-native";
+import {SwipeListView} from 'react-native-swipe-list-view';
 
 import {styles} from "./styles";
 import {theme} from '../../global/styles/theme';
@@ -7,6 +8,7 @@ import {theme} from '../../global/styles/theme';
 import swipePng from '../../assets/swipe.png';
 
 export function Cart() {
+    const [counterInput, setCounterInput] = useState(1);
 
     const foods = [
         {
@@ -14,7 +16,8 @@ export function Cart() {
             title: 'Veggie tomato mix',
             image: 'https://blog.duogourmet.com.br/wp-content/uploads/2019/07/37-Duo-Gourmet-sushi.jpg ',
             price: 'N5,900'
-        }, {
+        },
+        {
             id: '2',
             title: 'Egg and cucmber...',
             image: 'https://fortissima.com.br/wp-content/uploads/2015/03/comida-peruana-tt-width-640-height-420-bgcolor-FFFFFF.jpg',
@@ -28,9 +31,18 @@ export function Cart() {
         }
     ];
 
+    function handleCounterIncrease() {
+        setCounterInput(counterInput + 1);
+    }
+
+    function handleCounterDecrease(counter: number) {
+        setCounterInput(counterInput - 1);
+    }
+
+
     return (
-        <ScrollView style={styles.container}>
-            <View style={styles.top}>
+        <View style={styles.container}>
+            <View style={styles.head}>
                 <Image
                     style={styles.image}
                     source={swipePng}
@@ -38,6 +50,47 @@ export function Cart() {
                 />
                 <Text style={styles.textSwipe}>Swipe on an item to delete</Text>
             </View>
-        </ScrollView>
+            <SwipeListView
+                data={foods}
+                renderItem={(data, rowMap) => (
+                    <View style={styles.containerItem}>
+                        <View style={styles.item}>
+                            <Image
+                                source={{uri: data.item.image}}
+                                style={styles.imageItem}
+                            />
+                            <View style={styles.content}>
+                                <Text style={styles.title}>{data.item.title}</Text>
+                                <View style={styles.priceAmount}>
+                                    <Text style={styles.price}>{data.item.price}</Text>
+                                    <View style={styles.amount}>
+                                        <Button
+                                            onPress={() => handleCounterDecrease}
+                                            title='-'
+                                        />
+                                        <Text>{counterInput}</Text>
+                                        <Button
+                                            onPress={() => handleCounterIncrease}
+                                            title='+'
+                                        />
+                                    </View>
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+                )}
+                renderHiddenItem={(data, rowMap) => (
+                    <View style={styles.hiddenItem}>
+                        <View style={styles.heart}>
+                            <Text>A</Text>
+                        </View>
+                        <View style={styles.delete}>
+                            <Text>B</Text>
+                        </View>
+                    </View>
+                )}
+                rightOpenValue={-150}
+            />
+        </View>
     )
 }
--- src/screens/Cart/styles.ts
+++ src/screens/Cart/styles.ts
@@ -6,21 +6,78 @@ import {theme} from '../../global/styles/theme';
 export const styles = StyleSheet.create({
     container: {
         paddingTop: getStatusBarHeight(),
-        paddingHorizontal: 30,
         paddingBottom: 25,
     },
-    top:{
+    head: {
         flexDirection: 'row',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        paddingHorizontal: 30,
+        marginBottom: 20
     },
-    image:{
+    image: {
         width: 15,
         height: 15
     },
-    textSwipe:{
+    textSwipe: {
         fontFamily: theme.fonts.rounded600,
         color: theme.colors.black,
         fontSize: 10
+    },
+    containerItem: {
+        paddingHorizontal: 30,
+    },
+    item: {
+        backgroundColor: '#fff',
+        marginBottom: 15,
+        borderRadius: 20,
+        paddingHorizontal: 17,
+        paddingVertical: 16,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    imageItem: {
+        width: 69,
+        height: 69,
+        borderRadius: 69,
+        marginRight: 16
+    },
+    content: {
+
+    },
+    title:{
+        fontSize: 17,
+        color: theme.colors.black,
+        fontFamily: theme.fonts.rounded700,
+        marginBottom: 5
+    },
+    priceAmount:{
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    price:{
+        fontSize: 15,
+        color: theme.colors.primary,
+        fontFamily: theme.fonts.rounded600,
+    },
+    amount:{
+        paddingHorizontal: 5,
+        flexDirection: 'row',
+        backgroundColor: theme.colors.primary,
+        borderRadius: 5,
+        alignItems: 'center',
+    },
+    hiddenItem:{
+        paddingHorizontal:30,
+        flexDirection: 'row',
+        justifyContent: 'flex-end'
+    },
+    counter:{
+    },
+    heart:{
+
+    },
+    delete:{
+
     }
 })
